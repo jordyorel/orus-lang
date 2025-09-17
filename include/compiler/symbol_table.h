f1b9edcddeeaf78edf934598b37b61dfe340e409
Jordy Orel KONDA <jordyorel@protonmail.com>
2025-09-17 10:45:51 +0100

    Improve variable diagnostics and friendly error coverage

--- include/compiler/symbol_table.h
+++ include/compiler/symbol_table.h
@@ -19,13 +19,18 @@ typedef struct Symbol {
     bool is_mutable;                // Can be reassigned (mut vs immutable)
     bool is_initialized;            // Has been assigned a value
     bool is_arithmetic_heavy;       // Used in arithmetic operations frequently
-    
+
     // Symbol table management
     struct Symbol* next;            // Hash table collision chain
-    
+
     // Optimization hints
     int usage_count;                // Number of times symbol is accessed
     bool is_loop_variable;          // Used as loop induction variable
+
+    // Lifecycle tracking
+    SrcLocation declaration_location;   // Where the symbol was declared
+    SrcLocation last_assignment_location;// Most recent assignment location
+    bool has_been_read;                 // Has the value been read from
 } Symbol;
 
 // Simple hash table symbol table
@@ -42,10 +47,12 @@ SymbolTable* create_symbol_table(SymbolTable* parent);
 void free_symbol_table(SymbolTable* table);
 
 // Symbol management - DUAL REGISTER SYSTEM
-Symbol* declare_symbol_with_allocation(SymbolTable* table, const char* name, Type* type, 
-                                      bool is_mutable, struct RegisterAllocation* reg_alloc);
-Symbol* declare_symbol_legacy(SymbolTable* table, const char* name, Type* type, 
-                             bool is_mutable, int register_id);  // Compatibility
+Symbol* declare_symbol_with_allocation(SymbolTable* table, const char* name, Type* type,
+                                      bool is_mutable, struct RegisterAllocation* reg_alloc,
+                                      SrcLocation location, bool is_initialized);
+Symbol* declare_symbol_legacy(SymbolTable* table, const char* name, Type* type,
+                             bool is_mutable, int register_id,
+                             SrcLocation location, bool is_initialized);  // Compatibility
 Symbol* resolve_symbol(SymbolTable* table, const char* name);
 Symbol* resolve_symbol_local_only(SymbolTable* table, const char* name);
 
